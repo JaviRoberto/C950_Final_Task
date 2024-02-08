287f90631e36470be8ecc63bfb457fdfbeb3b874
jochoa70 <jocho70@wgu.edu>
2024-02-08 10:18:17 -0800

    test git link 2

---
+++ Main.py
@@ -0,0 +1,12 @@
+import Packages
+import csv
+import pandas as pd
+
+
+class Main:
+    print('hello')
+    test = Packages.Packages(1, 2, 3, 4, 5, 6, 7, 8)
+    test.print_package()
+
+
+
---
+++ packages.py
@@ -0,0 +1,28 @@
+
+class Packages:
+
+    print('Hello Package')
+
+    def __init__(self, ID, address, city, state, zipcode, deadline, kilo, status):
+        self.ID = ID
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.deadline = deadline
+        self.kilo = kilo
+        self.status = status
+
+    def print_package(self):
+        x = self.ID + self.address
+        print(self.ID)
+        print(self.address)
+        print(self.city)
+        print(self.state)
+        print(self.zipcode)
+        print(self.deadline)
+        print(self.kilo)
+        print(self.status)
+
+    #def status_set(self):
+
