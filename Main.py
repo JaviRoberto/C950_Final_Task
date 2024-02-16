3c6b605b415cfde9e688a1f18f65ff08773fc094
jochoa70 <jocho70@wgu.edu>
2024-02-16 14:46:07 -0800

    correct sort_packages, redo logic for delivery in main method.

--- Main.py
+++ Main.py
@@ -107,45 +107,67 @@ def get_packages():
         package_list.append(x)
     return package_list
 
-def sort_packages():
-    arr = get_packages()
+def sort_packages(arr):
     n = len(arr)
-    swapped = False
-    for i in range(1):
-        for i in range(n):
-            for j in range(0, n-i-2):
-                if find_distance(arr[j].address,arr[j+1].address) > find_distance(arr[j].address,arr[j+2].address):
-                    arr[j], arr[j+1] = arr[j+1], arr[j]
-                    swapped = True
-            if not swapped:
-                break
-        return arr
-
-
+    for i in range(n, n-1-3):
+        swapped = False
+        for j in range(0, n-i-3):
+            if find_distance(arr[j].address, arr[j+1].address) > find_distance(arr[j+2].address, arr[j+3].address):
+                arr[j], arr[j+2] = arr[j+2], arr[j]
+                arr[j+1], arr[j+3] = arr[j+3], arr[j+1]
+                swapped = True
+        if not swapped:
+            break
+    return arr
 
 
 class Main:
     package_list = get_packages()
+    print(package_list[14].address)
+    print(package_list[15].address)
 
     #print("Welcome to the WGUPS routing service by Javier Ochoa!")
     #input("Press Enter to sort packages into most efficient delivery pattern")
 
 
     truck1 = Trucks.Trucks(16, 18, 8, 0, 0, "4001 South 700 East", 1,
-                           [28,32,2,4,5,6,8,9,10,11,12,17,18,21,22,23,24,26,27,28])
-    # Packages #6, #25, #28, #32 arrived late on a flight and are not available to leave the hub before 9:05 a.m..
+                           [30,16,15,37,29,1,31,14,34,20,13,40]) #40 belongs here
 
     truck2 = Trucks.Trucks(16, 18, 8, 0, 0, "4001 South 700 East", 2,
-                           [1,13,14,15,16,19,20,3,18,36,38,37]) #40 is taken out!!!!!
-    # Packages #13, #14, #15. #16, #19, and #20 must go out for delivery on the same truck.
-    #15 needs to be delivered on or before 9:00 am
-    # Packages #3, #18, #36, and #38 may only be delivered by truck 2.
-
+                           [22,38,3,39,26,27,19,24,35,12,6,18,23,21,36,17])
 
     truck3 = Trucks.Trucks(16, 18, 8, 0, 0, "4001 South 700 East", 0,
-                           [6,9,25,29,30,31,33,34,35,37,39])
-    # Packages #1, 6, 9 13, 14, 16, 20, 25, 29, 30, 31, 34, 37 and 40 need to be delivered on or before 10:30 am
+                           [2,4,5,6,7,8,9,10,11,25,28,32,33])
 
+    #sorts truck3
+    truckarraytemp = truck1.package
+    temp_array = []
+    for pacakge in truckarraytemp:
+        temp_array.append(package_list[pacakge -1])
+    temp_sorted = sort_packages(temp_array)
+    for v in temp_sorted:
+       print(v.ID, end=",")
+    print(" ")
+
+    #sorts truck 2
+    truckarraytemp = truck2.package
+    temp_array = []
+    for pacakge in truckarraytemp:
+        temp_array.append(package_list[pacakge -1])
+    temp_sorted = sort_packages(temp_array)
+    for v in temp_sorted:
+        print(v.ID, end=",")
+    print("")
+
+    #sorts truck 3
+    truckarraytemp = truck3.package
+    temp_array = []
+    for pacakge in truckarraytemp:
+        temp_array.append(package_list[pacakge -1])
+    temp_sorted = sort_packages(temp_array)
+    for v in temp_sorted:
+        print(v.ID, end=",")
+    print("")
 
 # Two drivers and three trucks are available. So, no more than two trucks can be away from the hub at the same time.
 # The trucks move at a constant speed of 18 miles per hour or 0.3 miles per minute.
@@ -159,46 +181,62 @@ class Main:
 
 #TRUCK 1
     countz = 0.0
-    total_countz = 0.0
+    total_countz = 0.0000001
     truck_packages_array = truck1.get_packages()
     current_time = datetime.timedelta(0,0,0,0,0,8,)
-    for pacakge_number in truck_packages_array:
-        package_object = package_list[pacakge_number -1]
-        package_object2 = package_list[pacakge_number]
-        countz += float(find_distance(package_object.address,package_object2.address))
+    for i in range(1,len(truck_packages_array)):
+        package1 = truck_packages_array[i-1]
+        package2 = truck_packages_array[i]
+        package_object = package_list[package1-1]
+        package_object2 = package_list[package2-1] #WRONG, IT IS TAKING OBJECT FORM ARRAY
+        countz = float(find_distance(package_object.address,package_object2.address)) + .000000001
         total_countz += countz
-        current_time += datetime.timedelta(minutes=(countz/18))
+        current_time += datetime.timedelta(minutes=(countz*60/18))
         truck1.time_total = current_time
-        print("Truck 1 has delivered package",package_object.ID, "to", package_object.address, "at time", truck1.time_total)
-    print("Truck 1 total distance is",int(countz), "miles")
+        #print(find_distance(package_object.address,package_object2.address), "distance")
+        #print(package_object.ID, "==", package1,package_object2.ID,"==", package2)
+        print("Truck 1 has delivered package",package_object2.ID, "to", package_object.address, "at time", truck1.time_total)
+    print("Truck 1 total distance is",int(total_countz), "miles")
 
     countz = 0.0
+    total_countz = 0.0000001
     truck_packages_array = truck2.get_packages()
     current_time = datetime.timedelta(0,0,0,0,0,8,)
-    for pacakge_number in truck_packages_array:
-        package_object = package_list[pacakge_number -1]
-        package_object2 = package_list[pacakge_number]
-        countz += float(find_distance(package_object.address,package_object2.address))
-        current_time += datetime.timedelta(minutes=countz)
-        truck1.time_total = current_time
-        print("Truck 2 has delivered package",package_object.ID, "to", package_object.address, "at time", truck1.time_total)
-    print("Truck 2 total distance is",int(countz), "miles")
+    for i in range(1,len(truck_packages_array)):
+        package1 = truck_packages_array[i-1]
+        package2 = truck_packages_array[i]
+        package_object = package_list[package1-1]
+        package_object2 = package_list[package2-1] #WRONG, IT IS TAKING OBJECT FORM ARRAY
+        countz = float(find_distance(package_object.address,package_object2.address))
+        total_countz += countz
+        current_time += datetime.timedelta(minutes=(countz*60/18))
+        truck2.time_total = current_time
+        #print(find_distance(package_object.address,package_object2.address), "distance")
+        #print(package_object.ID, "==", package1,package_object2.ID,"==", package2)
+        print("Truck 2 has delivered package",package_object2.ID, "to", package_object.address, "at time", truck2.time_total)
+    print("Truck 2 total distance is",int(total_countz), "miles")
 
     countz = 0.0
+    total_countz = 0.0000001
     truck_packages_array = truck3.get_packages()
-    current_time = datetime.timedelta(0,0,0,0,0,8,)
-    for pacakge_number in truck_packages_array:
-        package_object = package_list[pacakge_number-1]
-        package_object2 = package_list[pacakge_number]
-        countz += float(find_distance(package_object.address,package_object2.address))
-        current_time += datetime.timedelta(minutes=countz)
-        truck1.time_total = current_time
-        print(countz, "Truck 3 has delivered package",package_object.ID, "to", package_object.address, "at time", truck1.time_total)
-    print("Truck 1 total distance is",int(countz), "miles")
+    current_time = datetime.timedelta(0,0,0,0,0,9,)
+    for i in range(1,len(truck_packages_array)):
+        package1 = truck_packages_array[i-1]
+        package2 = truck_packages_array[i]
+        package_object = package_list[package1-1]
+        package_object2 = package_list[package2-1] #WRONG, IT IS TAKING OBJECT FORM ARRAY
+        countz = float(find_distance(package_object.address,package_object2.address))
+        total_countz += countz
+        current_time += datetime.timedelta(minutes=(countz*60/18))
+        truck3.time_total = current_time
+        #print(find_distance(package_object.address,package_object2.address), "distance")
+        #print(package_object.ID, "==", package1,package_object2.ID,"==", package2)
+        print("Truck 3 has delivered package",package_object2.ID, "to", package_object.address, "at time", truck3.time_total)
+    print("Truck 3 total distance is",int(total_countz), "miles")
 
 
-for i in sort_packages():
-    print(i.ID, end=",")
+#for i in sort_packages():
+    #print(i.ID, end=",")
 
 
 
